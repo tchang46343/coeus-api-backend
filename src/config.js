f5f4ce9259cd7f0b8b61e233f006579568736800
Terrance Chang <tchang46343@gmail.com>
2019-11-19 15:19:10 -0800

    New info

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development"
+
+  API_BASE_URL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://coeus-systems-sand-sigma.now.sh/"
+  //NODE_ENV: process.env.NODE_ENV || "development"
 };
--- test/app.test.js
+++ test/app.test.js
@@ -1,21 +1,17 @@
-import { chai, supertest, mocha } from "../test/setup";
+//import { chai, supertest, mocha } from "./setup";
+const { expect } = require("chai");
+const supertest = require("supertest");
 const app = require("../src/app");
 const notValid = require("../src/app");
 
-describe(" GET /inventory should list all items in database", () => {
-  it('GET /inventory responds with 200 containing "JSON formatting of data base item"', () => {
+describe(" GET /inventory should list items in database", () => {
+  it("GET /inventory responds === with all database array objects ", () => {
     return supertest(app)
       .get("/inventory")
       .expect(200, "Current inventory list");
   });
-  it("should return an error message saying search term not valid", () => {
-    const a = madix;
-    const b = greuguhg;
-    const expectedAnswer = false;
-
-    const actualAnswer = notValid(a, b);
-
-    expect(expectedAnswer).to.equal(true, "2 === 3");
+  it("should return 400 if endpoint is not valid ", () => {
+    return supertest(app).get("/htrh");
   });
 });
 
