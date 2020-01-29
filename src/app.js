2714a809876e2c04c93bc765e8f18f9269dbe455
Terrance Chang <tchang46343@gmail.com>
2020-01-28 16:59:32 -0800

    Amended POST request

--- src/app.js
+++ src/app.js
@@ -47,57 +47,25 @@ app.get("/inventory", (req, res) => {
 ////////////////////////////////////////
 
 app.post("/inventory", (req, res) => {
+  const { vendor, item_name, description, price, availbility } = req.body;
+  const newvendor = { vendor, item_name, description, price, availbility };
+  for (const [key, value] of Object.entries(newvendor)) {
+    if (value == null) {
+      return res.status(400).json({
+        error: { message: `Missing '${key}' in request body` }
+      });
+    }
+  }
   db.insert(req.body)
     .returning("*")
     .into("newvendor")
     .then(data => {
       res.send(data);
+    })
+    .catch(error => {
+      res.status(500).json(error);
     });
 });
-
-//   const { vendor, item_name, description, price, availbility } = req.body;
-
-//   if (!vendor) {
-//     return res.status(400).send("Vendor required");
-//   }
-//   if (!item_name) {
-//     return res.status(400).send("Item ID required");
-//   }
-
-//   if (!description) {
-//     return res.status(400).send("description needed for clear interpretation!");
-//   }
-//   if (!price) {
-//     return res.status(400).send("Everything in life cost money!");
-//   }
-//   if (!availbility) {
-//     return res.status(400).send("Is there inventory in stock?");
-//   }
-
-//   if (vendor.length < 3 || vendor.length > 40) {
-//     return res.status(400).send("Vendor must be between 3 and 40 characters");
-//   }
-
-//   if (parseFloat(price).isNaN) {
-//     return res.status(400).send("price must be greater than $0.00");
-//   }
-
-//   const id = uuid();
-//   const newVendorSetup = {
-//     id,
-//     vendor,
-//     item_name,
-//     description,
-//     price,
-//     availbility
-//   };
-//   consumerData.push(newVendorSetup);
-
-//   res
-//     .status(201)
-//     .location(`http://localhost:8000/inventory/${id}`)
-//     .json(consumerData);
-// });
 
 ////////////////////////////////////////
 ////////////////////////////////////////
